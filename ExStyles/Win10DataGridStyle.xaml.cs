a37f1b0e9c8b853a491170994581c354e62d2234
Tekknow1580 <ebi.scherman@gmail.com>
2023-01-10 14:58:21 +0200

    New Styles

---
+++ ExStyles/Win10DataGridStyle.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Controls;
+using System.Reflection;
+
+namespace ExtraFunctions.ExStyles
+{
+    public partial class Win10DataGridStyle
+    {
+        int SelectedRow { get; set; } = -1;
+
+        void CelSelect(object sender, RoutedEventArgs e)
+        {
+            var Cell = sender as DataGridCell;
+            var GridDisplay = Cell.GetType().GetProperty("DataGridOwner", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(Cell) as DataGrid;
+            if (SelectedRow >= 0)
+            {
+                var PreRow = (DataGridRow)GridDisplay.ItemContainerGenerator.ContainerFromIndex(SelectedRow);
+                PreRow.ClearValue(Control.BackgroundProperty);
+                PreRow.ClearValue(Control.BorderBrushProperty);
+            }
+            SelectedRow = GridDisplay.Items.IndexOf(GridDisplay.SelectedCells[0].Item);
+            var Row = (DataGridRow)GridDisplay.ItemContainerGenerator.ContainerFromIndex(SelectedRow);
+            Row.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#b8cfde");
+            Row.BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#7c98ab");
+        }
+    }
+}
