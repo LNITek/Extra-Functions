6db56c5c7b0a5203e8258aad1ee4c4e105ccb71e
Tekknow1580 <ebi.scherman@gmail.com>
2023-02-10 09:27:05 +0200

    Minor Improvements

--- ExStyles/Win11DataGridStyle.xaml.cs
+++ ExStyles/Win11DataGridStyle.xaml.cs
@@ -14,24 +14,21 @@ namespace ExtraFunctions.ExStyles
     {
         void CelSelect(object sender, RoutedEventArgs e)
         {
-            var Cell = sender as DataGridCell;
-            var GridDisplay = Cell.GetType().GetProperty("DataGridOwner", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(Cell) as DataGrid;
+            var GridDisplay = ((DataGridCell)sender).GetType().GetProperty("DataGridOwner",
+                BindingFlags.NonPublic | BindingFlags.Instance).GetValue(sender) as DataGrid;
             var SelectedRow = -1;
-            try { int.TryParse(GridDisplay.Tag.ToString(), out SelectedRow); } catch { }
-            if (SelectedRow >= 0)
+            if (!string.IsNullOrWhiteSpace((GridDisplay.Tag ?? "").ToString()))
+                _ = int.TryParse(GridDisplay.Tag.ToString(), out SelectedRow);
+            if (SelectedRow >= 0 && SelectedRow < GridDisplay.Items.Count)
             {
-                try
-                {
-                    var PreRow = (DataGridRow)GridDisplay.ItemContainerGenerator.ContainerFromIndex(SelectedRow);
-                    PreRow.ClearValue(Control.BackgroundProperty);
-                    PreRow.ClearValue(Control.BorderBrushProperty);
-                }
-                catch { }
+                var PreRow = (DataGridRow)GridDisplay.ItemContainerGenerator.ContainerFromIndex(SelectedRow);
+                PreRow.ClearValue(Control.BackgroundProperty);
+                PreRow.ClearValue(Control.BorderBrushProperty);
             }
             SelectedRow = GridDisplay.Items.IndexOf(GridDisplay.SelectedCells[0].Item);
             var Row = (DataGridRow)GridDisplay.ItemContainerGenerator.ContainerFromIndex(SelectedRow);
-            Row.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#b8cfde");
-            Row.BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#7c98ab");
+            Row.Background = Brushes.LightGray;
+            Row.BorderBrush = Brushes.Gray;
             GridDisplay.Tag = SelectedRow;
         }
     }
